a4d32784fd6e1bfc1a81deb6283d98d0bb78660c
sptraardiansyah <sptraardiansyah24@gmail.com>
2026-04-08 13:33:18 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main (String[]args) {
+        Mobil mobilSaya = new Mobil("Toyota");
+
+        mobilSaya.Gas();
+        mobilSaya.Nyalakan();
+        mobilSaya.Gas();
+        mobilSaya.Gas();
+        mobilSaya.Rem();
+        mobilSaya.Nyalakan();
+        mobilSaya.Matikan();
+
+    }
+}
